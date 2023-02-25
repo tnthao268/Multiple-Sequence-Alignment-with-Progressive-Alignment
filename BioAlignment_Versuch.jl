d61869770327a51a97dfc841509f306a0cdd6844
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-02-25 13:57:41 +0700

    create macro dna_str

--- BioAlignment_Versuch.jl
+++ BioAlignment_Versuch.jl
@@ -1,3 +1,25 @@
 using Pkg
 Pkg.add("BioAlignments")
 using BioAlignments
+
+#macro to check, if the sequence is a dna
+macro dna_str(dna)
+    return quote
+        nu = "ATGC" #indexes of a string is Char-Type
+        
+        #when there is a index, which isn't a DNA nucleotide
+        for i ∈ $(esc(dna)) # \in
+            if i ∉ nu # \notin
+                return 0
+            end
+        end
+        return $(esc(dna))
+    end
+end
+
+#Using macro dna
+dna"ATAV"
+dna"ATAC"
+
+@dna_str "ATTA"
+@dna_str "AXT"
