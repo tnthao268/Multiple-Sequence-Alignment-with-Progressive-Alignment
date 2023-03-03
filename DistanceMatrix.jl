49e0da1f1bb45e2731bcbec1309844e0ae46d581
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-03 10:45:55 +0700

    add score in all positions in Distance Matrix, instead of a half

--- DistanceMatrix.jl
+++ DistanceMatrix.jl
@@ -1,6 +1,7 @@
+#Create Distance Matrix
 include("ReadAlignment.jl")
-
-#=  create a distance matrix from scores of alignments of sequences
+    
+#=  create a distance matrix from scores of pairwise alignments of sequences
     return distance matrix
 =#
 function createDistanceMatrix(dict_seqs::Dict{String,String})
@@ -12,7 +13,7 @@ function createDistanceMatrix(dict_seqs::Dict{String,String})
         for j in i+1:len
             dict_pair = filter(s -> s.first == names_list[i] || s.first == names_list[j], dict_seqs)
             aln = readDNAAlignment(dict_pair)
-            dm[i,j] = aln.score
+            dm[i,j] = dm[j,i] = aln.score
         end
     end
     dm
