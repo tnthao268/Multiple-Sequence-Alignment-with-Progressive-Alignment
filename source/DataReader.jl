9c7bb17b1e5487bfda991d8502a24f13101c8c32
tntha <tnthao26899@gmail.com>
2023-03-12 21:00:45 +0100

    Add method readSequences applied for many fasta files

--- source/DataReader.jl
+++ source/DataReader.jl
@@ -10,6 +10,9 @@ mutable struct Record
     sequence :: AbstractString
 end
 
+# input_files_names = []
+
+# function to return list of records based on name of the fasta filen(input_file_name)
 function readSequences(input_file_name :: AbstractString)
     records = Record[] # records is type FastaRecord
     FastaReader(input_file_name) do fr
@@ -21,4 +24,20 @@ function readSequences(input_file_name :: AbstractString)
     records
 end
 
+# function to return a list of records based on a list of fasta files
+function readSequences_file(input_files_names :: Vector{String}) # input_files_names is list of fasta files' names
+    records = Record[] # records is type FastaRecord
+    for name in input_files_names
+        FastaReader(name) do fr
+            for (desc, seq) in fr
+                push!(records,Record("$desc","$seq"))
+            
+             end
+        end
+    end
+    records
+end
 
+
+
+# print(sequences)
