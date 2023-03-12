fcd57da6f1c772e0d7ed30daa5f8ceab06cfda73
tntha <tnthao26899@gmail.com>
2023-03-12 21:02:02 +0100

    Change input after Adding method readSequences applied for many fasta files

--- Project_Example.jl
+++ Project_Example.jl
@@ -3,7 +3,7 @@ include("source/DataReader.jl")
 # include("source/DataWriter.jl")
 # include("source/UPGMA.jl")
 
-sequence1 = readSequences("data/ChimpanzeeDND1.fasta")
-sequence2 = readSequences("data/DogDND1.fasta")
-sequence3 = readSequences("data/HomoSapienDND1.fasta")
-sequence4 = readSequences("data/MouseDND1.fasta")
+input_files_names = ["data/ChimpanzeeDND1.fasta","data/DogDND1.fasta","data/HomoSapienDND1.fasta","data/MouseDND1.fasta"]
+
+records_sequences = readSequences_file(input_files_names)
+
