bb5c418bae87ddc342e6a50a1224afad67f8cbc1
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-13 03:21:34 +0700

    create DistanceMatrix and Dictionary

--- Project_Example.jl
+++ Project_Example.jl
@@ -1,9 +1,14 @@
-# include("source/MultiSequencesAlignment.jl")
-include("source/DataReader.jl")
-# include("source/DataWriter.jl")
-# include("source/UPGMA.jl")
+include("source/MultiSequencesAlignment.jl")
+
 
 input_files_names = ["data/ChimpanzeeDND1.fasta","data/DogDND1.fasta","data/HomoSapienDND1.fasta","data/MouseDND1.fasta"]
 
 records_sequences = readSequences_file(input_files_names)
 
+#createDistanceMatrix
+distanceMatrix = createDistanceMatrix(records_sequences)
+
+#create Dictionary and leaf_names
+dict = createDictionary(records_sequences)
+dict_records = dict.dict_records
+leaf_names = dict.leaf_names
