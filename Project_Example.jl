cbf3d3d58fab30d685cc3b703a3165b99e73b76e
tntha <tnthao26899@gmail.com>
2023-03-12 20:38:57 +0100

    Add example of DataReader

--- Project_Example.jl
+++ Project_Example.jl
@@ -1,1 +1,9 @@
-include("source/MultiSequencesAlignment.jl")
+# include("source/MultiSequencesAlignment.jl")
+include("source/DataReader.jl")
+# include("source/DataWriter.jl")
+# include("source/UPGMA.jl")
+
+sequence1 = readSequences("data/ChimpanzeeDND1.fasta")
+sequence2 = readSequences("data/DogDND1.fasta")
+sequence3 = readSequences("data/HomoSapienDND1.fasta")
+sequence4 = readSequences("data/MouseDND1.fasta")
