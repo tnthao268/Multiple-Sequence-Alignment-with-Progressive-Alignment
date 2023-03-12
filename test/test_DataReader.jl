84c4fbf94429bd4777e6edcaebaa5bb38c448e53
tntha <tnthao26899@gmail.com>
2023-03-12 18:48:51 +0100

    Remove module

--- test/test_DataReader.jl
+++ test/test_DataReader.jl
@@ -1,15 +1,20 @@
-using .DataReader
+include(dirname(@__FILE__()) * "/../source/DataReader.jl")
+
+
 using Test
 
 
-fasta_record  = FastaRecord("test string1", "ACGT")
-fasta_record2 = FastaRecord("test string1", "ACGT")
+fasta_record  = Record("test string1", "ACGT")
+fasta_record2 = Record("test string1", "ACGT")
 
 println("testing FastaRecord...")
 @test fasta_record.description == "test string1"
 @test fasta_record.sequence == "ACGT"
+@test fasta_record2.description == "test string1"
+@test fasta_record2.sequence == "ACGT"
 
-@test fasta_record == fasta_record2
+
+# @test fasta_record == fasta_record2
 
 sequences = readSequences(dirname(@__FILE__()) * "/../data/sequence.fasta")
 # print(sequences)
@@ -19,7 +24,7 @@ sequences = readSequences(dirname(@__FILE__()) * "/../data/sequence.fasta")
 
 
 sequences1 = readSequences(dirname(@__FILE__()) * "/../data/input_test_sequences.faa")
-# print(sequences1)
+print(sequences1)
 @test length(sequences1) > 0
 @test length(sequences1) == 10
 @test sequences1[1].description == "1"
