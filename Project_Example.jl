c4598bb959d0c578642c9ab24be98e0b6f55665e
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-14 00:22:22 +0700

    add create guildtree and do msa

--- Project_Example.jl
+++ Project_Example.jl
@@ -1,6 +1,6 @@
 include("source/MultiSequencesAlignment.jl")
 
-
+#read file and create list of Records
 input_files_names = ["data/ChimpanzeeDND1.fasta","data/DogDND1.fasta","data/HomoSapienDND1.fasta","data/MouseDND1.fasta"]
 
 records_sequences = readSequences_file(input_files_names)
@@ -12,3 +12,11 @@ distanceMatrix = createDistanceMatrix(records_sequences)
 dict = createDictionary(records_sequences)
 dict_records = dict.dict_records
 leaf_names = dict.leaf_names
+
+#create GuildTree
+cluster = upgma(distanceMatrix, leaf_names)
+guildtree = guildTreeInstruction(cluster)
+last_instruction = last(guildtree)
+
+#progressive alignment
+new_dict_records = align1(last_instruction,dict_records) #lasts so long
