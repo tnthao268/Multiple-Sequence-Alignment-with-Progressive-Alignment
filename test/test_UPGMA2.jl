24f07fc062b4b9025da9be892cc1640254802062
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-13 23:23:04 +0700

    add test_UPGMA2.jl

---
+++ test/test_UPGMA2.jl
@@ -0,0 +1,13 @@
+include("../test/test_DistanceMatrix.jl")
+using Test
+
+records
+dm = createDistanceMatrix(records)
+dict = createDictionary(records)
+dict_records = dict.dict_records
+leaf_names = dict.leaf_names
+
+cluster = upgma(dm,leaf_names)
+guildTree = guildTreeInstruction(cluster)
+test_guildTree = [["A", "D"], [["A", "D"], "C"], ["B", "E"],[[["A", "D"], "C"], ["B", "E"]]]
+@test guildTree == test_guildTree
