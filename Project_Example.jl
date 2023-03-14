877a35a50d25a03a764fa8c65ce572bbc8aaf8f0
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-14 16:41:49 +0700

    add step Progressive Alignment and DataWriter

--- Project_Example.jl
+++ Project_Example.jl
@@ -16,7 +16,15 @@ leaf_names = dict.leaf_names
 #create GuildTree
 cluster = upgma(distanceMatrix, leaf_names)
 guildtree = guildTreeInstruction(cluster)
-last_instruction = last(guildtree)
 
-#progressive alignment
-new_dict_records = align1(last_instruction,dict_records) #lasts so long
+#there are 2 ways to align multi sequences
+# first: progressive alignment with a simplified instruction from the guild tree in list
+simpleInstruction = simpleGuildTree(guildtree)
+@time new_dict_records = progressiveAlignment(simpleInstruction,dict_records) #lasts so long
+
+#second: progressive alignment with a nested instruction from the guild tree
+nestedInstruction = getNestedInstruction(guildtree)
+@time new_dict_records = progressiveAlignment2(nestedInstruction) #lasts maybe shorter, but still long
+
+#Data Writer: Write the result of multi sequences alignment in a text file (.txt)
+writeSequences("example.txt",new_dict_records,100)
