609c7922f4fbd00de9ccc5993968f491acdc91b1
tramnguyen <dq.tram.nguyen.99@gmail.com>
2023-03-20 10:39:14 +0700

    add using Progressive Alignment 2 in the example

--- example/Example.jl
+++ example/Example.jl
@@ -16,21 +16,21 @@ leaf_names = dict.leaf_names
 
 #create GuildTree
 cluster = upgma(distanceMatrix, leaf_names)
-
 print_ascii(cluster) # easy-to-view version of tree
-
-
 guildtree = guildTreeInstruction(cluster)
 
-
- # progressive alignment with a simplified instruction from the guild tree in list
+#Progressive Alignment: There are two ways to run it and they return the same result
+#First one: progressive alignment with a simplified instruction from the guild tree in list
 simpleInstruction = simpleGuildTree(guildtree)
 @time new_dict_records = progressiveAlignment(simpleInstruction,dict_records)
 
+#Second one: progressive alignment with a nested instruction from the guild tree
+nestedinstruction = nestedInstruction(guildtree)
+@time new_dict_records2 = progressiveAlignment2(nestedinstruction, dict_records)
 
 #Data Writer: Write the result of multi sequences alignment in a text file (.txt)
 
 # first method of aligning
 writeSequences("example/example.txt",new_dict_records,50)
-
+writeSequences("example/example2.txt", new_dict_records2, 50)
 
